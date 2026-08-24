73000fa24a78271f9f115a603ab3d6f0fb17929c
elpeco123 <franchescocivetta@gmail.com>
2026-08-24 17:21:28 -0300

    ultima solucion vercel 4

--- chirper/app/Providers/AppServiceProvider.php
+++ chirper/app/Providers/AppServiceProvider.php
@@ -3,8 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-
 use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -22,5 +22,9 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Paginator::useBootstrapFive();
+
+        if (config('app.env') === 'production') {
+            URL::forceScheme('https');
+        }
     }
 }
